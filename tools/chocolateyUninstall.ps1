8166c91680d754748dffc51179f86d44f38abd9b
Brent Anduaga-Arias <Brent Anduaga-Arias>
2016-03-08 23:36:45 -0700

    fix uninstall script

--- tools/chocolateyUninstall.ps1
+++ tools/chocolateyUninstall.ps1
@@ -1,3 +1,3 @@
 . (Join-Path (Split-Path -parent $MyInvocation.MyCommand.Definition) chocolateyInclude.ps1)
 
-Uninstall-ChocolateyZipPackage "$packageName" "${packageName}Install.zip"
+Uninstall-ChocolateyZipPackage "$packageName" "$packageName.zip"
